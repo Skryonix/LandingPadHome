a902b3274eb5d0dc509af06457c343e5f4163d8d
Skryonix <143288032+Skryonix@users.noreply.github.com>
2023-11-07 09:20:51 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,72 +1,27 @@
 let currentSlide = 0;
 
 function updateCarousel() {
-  const slides = document.querySelectorAll(".carousel-slide img");
-  const slideWidth = slides[0].clientWidth;
-  const gap = 20;
+  const slides = document.querySelectorAll(".carousel-game");
+  if (slides.length === 0) {
+    console.error("No slides found. Make sure your HTML contains elements with class '.carousel-game'.");
+    return;
+  }
 
+  // Use the width of the first slide as the width for all slides
+  const slideWidth = slides[0].offsetWidth;
+  const gap = 20; 
   const maxSlideIndex = slides.length - 1;
 
-  if (currentSlide > maxSlideIndex) currentSlide = 0;
-  if (currentSlide < 0) currentSlide = maxSlideIndex;
+  // Update currentSlide to stay within bounds
+  currentSlide = (currentSlide > maxSlideIndex) ? 0 : (currentSlide < 0) ? maxSlideIndex : currentSlide;
 
-  const moveDistance = slideWidth + gap;
-  document.querySelector(".carousel-slide").style.transform = `translateX(${
-    -moveDistance * currentSlide
-  }px)`;
-}
-
-function tiltOnMouseMove(event) {
-    const slide = event.currentTarget;
-    const slideWidth = slide.offsetWidth;
-    const slideHeight = slide.offsetHeight;
-    const tiltMax = 15; 
-    const offsetX = (event.clientX - slide.getBoundingClientRect().left) - slideWidth / 2;
-    const offsetY = (event.clientY - slide.getBoundingClientRect().top) - slideHeight / 2;
-
-    const tiltX = (offsetY / slideHeight * 2) * tiltMax;
-    const tiltY = -(offsetX / slideWidth * 2) * tiltMax;
-
-    const boxShadowEffect = `${-tiltY / 4}px ${tiltX / 4}px 30px rgba(0, 0, 0, 0.4)`;  // Increased shadow for more depth
-
-    slide.style.transform = `perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale(1.1)`;  // Scale(1.1) to make the game box pop up
-    slide.style.boxShadow = boxShadowEffect;
-}
-
-function removeTiltEffect(image) {
-    image.style.transform = "scale(1)"; // Resetting the scale to normal
-    image.style.boxShadow = "0 4px 15px rgba(0, 0, 0, 0.2)"; // Resetting the shadow to normal
+  // Calculate total distance to move
+  const moveDistance = (slideWidth + gap) * currentSlide;
+  document.querySelector(".carousel-slide").style.transform = `translateX(${-moveDistance}px)`;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Dark mode toggle functionality
-  const darkModeToggle = document.getElementById("darkModeToggle");
-  const gif = document.querySelector(".header img");
-  const body = document.body;
-
-  function toggleDarkMode() {
-    if (body.classList.contains("dark-mode")) {
-      body.classList.remove("dark-mode");
-      localStorage.setItem("dark-mode", "disabled");
-      darkModeToggle.innerText = "Dark Mode";
-    } else {
-      body.classList.add("dark-mode");
-      localStorage.setItem("dark-mode", "enabled");
-      darkModeToggle.innerText = "Light Mode";
-    }
-  }
-
-  if (localStorage.getItem("dark-mode") === "enabled") {
-    body.classList.add("dark-mode");
-    darkModeToggle.innerText = "Light Mode";
-  } else {
-    darkModeToggle.innerText = "Dark Mode";
-  }
-
-  darkModeToggle.addEventListener("click", toggleDarkMode);
-  gif.addEventListener("click", toggleDarkMode);
-
-  // Carousel functionality
+  // Carousel controls
   const prevButton = document.querySelector(".prev-btn");
   const nextButton = document.querySelector(".next-btn");
 
@@ -80,70 +35,101 @@ document.addEventListener("DOMContentLoaded", function () {
     updateCarousel();
   });
 
+  // Initialize the carousel position
   updateCarousel();
+});
+// Dark mode toggle functionality
+function toggleDarkMode() {
+  const body = document.body;
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  body.classList.toggle("dark-mode");
+  const isDarkMode = body.classList.contains("dark-mode");
+  localStorage.setItem("dark-mode", isDarkMode ? "enabled" : "disabled");
+  darkModeToggle.innerText = isDarkMode ? "Light Mode" : "Dark Mode";
+}
 
-  // Contact form validation
-  const form = document.querySelector("#contactForm");
-  const inputs = form.querySelectorAll("input, textarea");
-  const submitButton = form.querySelector("input[type='submit']");
-  const successMessage = document.createElement("p");
-  successMessage.id = "successMessage";
-  form.insertBefore(successMessage, form.firstChild);
-
-  function displayError(input, message) {
-    const errorMessage = input.nextElementSibling;
-    errorMessage.textContent = message;
-    input.style.borderColor = "red";
+// Get the weather data from OpenWeatherMap API
+async function getWeather() {
+  if (!navigator.geolocation) {
+    console.error("Geolocation is not supported by your browser");
+    return;
   }
 
-  function validateInput(input) {
-    if (!input.checkValidity()) {
-      displayError(input, input.validationMessage);
-      return false;
-    } else {
-      input.nextElementSibling.textContent = "";
-      input.style.borderColor = "initial";
-      return true;
-    }
+  function success(position) {
+    const apiKey = '2cafd8e15fd7c6f38c757a64acf77bea';
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        displayWeather(data);
+      })
+      .catch(error => {
+        console.error("Error fetching weather data:", error);
+      });
   }
 
-  function checkFormValidity() {
-    let isValid = true;
-    inputs.forEach((input) => {
-      if (!validateInput(input)) {
-        isValid = false;
-      }
-    });
-    return isValid;
+  function error(err) {
+    console.error(`Geolocation error (${err.code}): ${err.message}`);
   }
 
-  inputs.forEach((input) => {
-    input.addEventListener("input", function () {
-      validateInput(input);
-      submitButton.disabled = !checkFormValidity();
-    });
-  });
+  navigator.geolocation.getCurrentPosition(success, error);
+}
 
-  form.addEventListener("submit", function (event) {
-    if (!checkFormValidity()) {
-      event.preventDefault();
-    } else {
-      successMessage.textContent = "Form submitted successfully!";
-      form.reset();
-      setTimeout(() => {
-        successMessage.textContent = "";
-      }, 5000);
-    }
-  });
+function displayWeather(data) {
+  const cityElem = document.getElementById('weather-city');
+  const tempElem = document.getElementById('weather-temp');
+  const descElem = document.getElementById('weather-desc');
+  const iconElem = document.getElementById('weather-icon');
 
-  submitButton.disabled = true;
+  if (!cityElem || !tempElem || !descElem || !iconElem) {
+    console.error("One or more weather display elements are missing in the HTML structure.");
+    return;
+  }
 
-  // Attach omnidirectional tilt effect to each carousel image
+  cityElem.textContent = data.name;
+  tempElem.textContent = `${data.main.temp.toFixed(1)}°C`;
+  descElem.textContent = data.weather[0].description;
+  iconElem.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  iconElem.alt = data.weather[0].description;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize all functionalities
+
+  // Weather initialization
+  getWeather();
+
+  // Dark Mode initialization
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  if (!darkModeToggle) {
+    console.error("Dark mode toggle button not found.");
+    return;
+  }
+  const gif = document.querySelector(".header img");
+  if (!gif) {
+    console.error("Header image for dark mode toggle not found.");
+    return;
+  }
+  const darkModeEnabled = localStorage.getItem("dark-mode") === "enabled";
+  document.body.classList.toggle("dark-mode", darkModeEnabled);
+  darkModeToggle.innerText = darkModeEnabled ? "Light Mode" : "Dark Mode";
+
+  // Tilt effect on carousel images
   const slides = document.querySelectorAll(".carousel-slide img");
-  slides.forEach((slide) => {
+  if (slides.length === 0) {
+    console.error("No carousel images found for tilt effect.");
+    return;
+  }
+  slides.forEach(slide => {
     slide.addEventListener("mousemove", tiltOnMouseMove);
-    slide.addEventListener("mouseleave", function() {
-      removeTiltEffect(slide);
-    });
+    slide.addEventListener("mouseleave", removeTiltEffect);
   });
 });
